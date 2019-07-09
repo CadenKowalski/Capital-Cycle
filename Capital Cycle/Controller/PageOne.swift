52b5c40a72eb75b1c4fffba1de9a2d684cdd8a1d
JeffTheShoe <cadenkowalski1@gmail.com>
2019-07-08 20:34:29 -0400

    Added data and scroll functionality to page one

--- Capital Cycle/Controller/PageOne.swift
+++ Capital Cycle/Controller/PageOne.swift
@@ -7,17 +7,53 @@
 //
 
 import UIKit
+import SafariServices
 
 class PageOne: UIViewController {
 
     @IBOutlet weak var gradientView: UIView!
+    @IBOutlet weak var scrollViewDisplay: UIView!
+    @IBOutlet weak var campDatesLbl: UILabel!
+    @IBOutlet weak var signUpBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setLayout()
+        CustomizeLayout()
     }
     
-    func setLayout() {
+    // Formats the UI
+    func CustomizeLayout() {
+        // Adds gradients
         gradientView.setTwoGradientBackground(colorOne: Colors.Orange, colorTwo: Colors.Purple)
+        signUpBtn.setTwoGradientButton(colorOne: Colors.Orange, colorTwo: Colors.Purple, cornerRadius: 30)
+    }
+    
+    // MARK: Actions
+    
+    // Takes the user to the CapitalCycleCamp Facebook page
+    @IBAction func Facebook(_ sender: UIButton) {
+        let facebookAppURL = URL(string: "fb://profile/839679162770435")!
+        if UIApplication.shared.canOpenURL(facebookAppURL) {
+            UIApplication.shared.open(facebookAppURL, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.open(URL(string: "https://www.facebook.com/CapitalCycleCamp/")!, options: [:], completionHandler: nil)
+        }
+    }
+    
+    // Takes the user to the CapitalCycleCamp Instagram page
+    @IBAction func Instagram(_ sender: UIButton) {
+        let instagramURL = NSURL(string: "instagram://capitalcyclecamp/")! as URL
+        if UIApplication.shared.canOpenURL(instagramURL as URL) {
+            UIApplication.shared.open(instagramURL, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.open(NSURL(string: "https://instagram.com/capitalcyclecamp/")! as URL)
+        }
+    }
+    
+    // Takes the user to the Capital Cycle Camp sign up page
+    @IBAction func SignUp(_ sender: UIButton) {
+        let websiteURL = NSURL(string: "https://capitalcyclecamp.org/signup")! as URL
+        let svc = SFSafariViewController(url: websiteURL)
+        present(svc, animated: true, completion: nil)
     }
 }
